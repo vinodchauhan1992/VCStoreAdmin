41ac5ee97e5258e2d27a689b6d52c0735d4ffac7
vinodchauhan1992 <69452412+vinodchauhan1992@users.noreply.github.com>
2024-02-23 04:38:07 +0530

    completed MOST OF THE THINGS

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -1,5 +1,5 @@
 export default {
-  isLocalHostEnvironment: true,
-  localHost: 'http://localhost:3371/',
-  productionHost: 'https://vcstoreapi-production.up.railway.app/'
+  isLocalHostEnvironment: false,
+  localHost: 'http://localhost:3371/v1/',
+  productionHost: 'https://vcstoreapi-production.up.railway.app/v1/'
 }
